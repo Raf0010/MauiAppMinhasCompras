daf3279f892c69ff1ad4bcb4120baef59e079bb0
EEEE <EEEE@LAPTOP-4J4A9CQR>
2025-03-31 09:03:32 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
